9f0397befce21c501d7ee374ba89b4fc78eaaff4
yash <ghorpade205@gmail.com>
2026-04-17 21:09:32 +0530

    fix: use env var for API baseURL instead of hardcoded localhost

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://yash9970-movie-backend.hf.space",
 });
 
 api.interceptors.request.use((config) => {
